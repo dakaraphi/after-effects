148e4527ee98c2960220c887d0c756a2279e63ff
Dakara <chad.meadows@gmail.com>
2019-06-02 16:42:40 -0400

    new prototype library

--- index.js
+++ index.js
@@ -1,241 +1,87 @@
 'use strict';
 
-/*******************************************************************/
-// DEPENDENCIES
-/*******************************************************************/
+const fs = require('fs');
+const path = require('path');
+const afterEffectsExecutor = require('./lib/process-execution.js');
+const makeTransformGenerator = require('./lib/script-transform-generator').makeTransformGenerator;
 
-const os = 		  require('os'),
-      fs =      require('fs'),
-      path =    require('path'),
-      is =      require('is-explicit').default,
-      uuid = 		require('uuid'),
-      Command = require('./lib/command');
+const Tail = require('tail').Tail;
 
-/*******************************************************************/
-// SETUP
-/*******************************************************************/
-
-const options = {
-	errorHandling: true,
-	minify: false,
-  program: null,
-  includes: [
-		path.join(__dirname, '/lib/includes/console.jsx'),
-		path.join(__dirname, '/lib/includes/es5-shim.jsx'),
-		path.join(__dirname, '/lib/includes/get.jsx')
-	]
+let options = {
+    afterEffectsPath: null,
+    includes: [
+        path.join(__dirname, '/lib/includes/es5-shim.jsx'),
+        path.join(__dirname, '/lib/includes/es6-shim.js'),
+        path.join(__dirname, '/lib/includes/json2.js'),
+        path.join(__dirname, '/lib/includes/ae-lib.js'),
+    ]
 };
 
-const platform = (() => {
+function tailLogToConsole() {
+    const logFile = path.join(process.cwd(), "after-effects-script.log")
+    console.log('creating log file: ' + logFile)
+    fs.writeFileSync(logFile, '') // create or clear previous log
+    const tailOptions = { fromBeginning: true }
+    const tail = new Tail(logFile, tailOptions);
 
- const platform_name = os.platform();
- if (platform_name === 'darwin') //mac
-   return require('./lib/platform-mac');
-
- else if (platform_name.includes('win')) { //windows 32 or 64
-   return require('./lib/platform-win');
-
- } else
-   throw new Error(Errors.UnsupportedPlatform);
-
-})();
-
-/*******************************************************************/
-// ERRORS
-/*******************************************************************/
-
-const Errors = {
-  UnsupportedPlatform : 'Cannot run After Effects commands in an environment it can\'t be installed in.',
-  BadExecuteArgument : 'execute expects a function or AfterEffectsCommand instance.',
-  ApplicationNotFound : 'Cannot execute command, After Effects could not be found in your application directory. Install After Effects in your application directory, or provide a path in program option.',
-  NoResult : 'Could not get results from After Effects. Ensure that Preferences > General > Allow Scripts to Write Files and Access Network is enabled.'
-};
-
-class AfterEffectsError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'AfterEffectsError';
-  }
-}
-
-/*******************************************************************/
-// HELPER
-/*******************************************************************/
-
-function prepare_command(input_args) {
-
-  let command = null;
-  const args = Array.prototype.slice.call(input_args);
-  const funcOrCommand = args.shift();
-
-  if (is(funcOrCommand, Command))
-    command = funcOrCommand;
-
-  if (is(funcOrCommand, Function))
-    command = new Command(funcOrCommand);
-
-  if (!command)
-    throw new Error(Errors.BadExecuteArgument);
-
-  command.arguments = args;
-  command.result_file = null;
-
-  return command;
-
-}
-
-function ensure_executable(command) {
-  if (!platform.canExecute(command))
-    throw new Error(Errors.ApplicationNotFound);
-}
-
-function prepare_script_path(scriptPath, command) {
-  if (!path.isAbsolute(scriptPath))
-    scriptPath = path.resolve(platform.scriptsDir(command), scriptPath);
-
-  if (path.extname(scriptPath) === '')
-    scriptPath += '.jsx';
-
-  return scriptPath;
-}
-
-function create_result_file_name(command) {
-  command.result_file = `ae-result-${uuid.v4()}.js`;
-}
-
-function get_results(command) {
-  if (!is(command.result_file, String))
-    return;
-
-  let results = {};
-
-  try {
-    //For macs, the javascript function inside After Effects that points toward
-    //the operating systems temp folder is slightly different than os.tmpdir,
-    //having a 'TemporaryItems' subfolder.
-    const sub_temp_dir = os.platform() === 'darwin' ? 'TemporaryItems' : '';
-    const jsfile = path.join(os.tmpdir(), sub_temp_dir, command.result_file);
-    results = require(jsfile);
-
-    fs.unlink(jsfile, function(err) {
-      if (err)
-        console.error (err)
+    tail.on("line", function (data) {
+        console.log(data);
     });
 
-    command.result_file = null;
-  } catch (err) {
-
-    command.result_file = null;
-    return err;
-  }
-  if (is(results.logs, Array))
-    results.logs.forEach(log => process.stdout.write(`${log}\n`));
-
-  return results;
-}
-
-/*******************************************************************/
-// INTERFACE
-/*******************************************************************/
-
-function execute(/*args*/) {
-
-  const command = prepare_command(arguments);
-  ensure_executable(command);
-  create_result_file_name(command);
-
-  return platform.execute(command)
-  //Handle Results
-  .then(() => new Promise((resolve,reject) => {
-
-    const results = get_results(command);
-    if (results == null)
-      resolve();
-
-    if (is(results, Error))
-      reject(Errors.NoResult);
-
-    if (is(results.returned, Error))
-      reject(new AfterEffectsError(results.returned.message));
-    else
-      resolve(results.returned);
-  }));
-}
-
-function executeSync(/*args*/) {
-
-  const command = prepare_command(arguments);
-  ensure_executable(command);
-  create_result_file_name(command);
-
-  platform.executeSync(command);
-  const results = get_results(command);
-
-  //Handle results
-  if (results == null)
-    return;
-
-  if (is(results, Error))
-    throw new Error(Errors.NoResult);
-
-  if (is(results.returned, Error))
-    throw new AfterEffectsError(results.returned.message);
-  else
-    return results.returned;
-}
-
-function create(funcOrCommand, scriptPath) {
-
-  //prepare command args shouldn't include scriptPath
-  const args = Array.prototype.slice.call(arguments, 2);
-  args.unshift(funcOrCommand);
-
-  const command = prepare_command(args);
-  scriptPath = prepare_script_path(scriptPath, command);
-
-  return new Promise((resolve, reject) => {
-    fs.writeFile(scriptPath, command.toString(), 'utf-8', (err) => {
-      if (err)
-        reject(err);
-      else
-        process.stdout.write(`Script written to ${scriptPath}\n`);
-        resolve(scriptPath);
+    tail.on("error", function (error) {
+        console.log('ERROR: ', error);
     });
-  });
+
+    return tail;
 }
 
-function createSync(funcOrCommand, scriptPath) {
-  //prepare command args shouldn't include scriptPath
-  const args = Array.prototype.slice.call(arguments, 2);
-  args.unshift(funcOrCommand);
+function executeFunctionInAfterEffects(afterEffectsFn, ...parameters) {
 
-  const command = prepare_command(args);
-  scriptPath = prepare_script_path(scriptPath, command);
+    const tail = tailLogToConsole()
 
-  fs.writeFileSync(scriptPath, command.toString(), 'utf-8');
+    const transformGenerator = makeTransformGenerator(afterEffectsFn, parameters, options)
+    const scriptContent = transformGenerator.makeSource();
+    const scriptFile = transformGenerator.generatedScriptFile();
+    const resultFile = transformGenerator.resultsFile();
 
-  process.stdout.write(`Script written to ${scriptPath}\n`);
-  return scriptPath;
+    try {
+        console.log('Created script for execution: ' + scriptFile)
+        fs.writeFileSync(scriptFile, scriptContent, 'utf-8');
+    } catch (err) {
+        throw Error('Could not create jsx script for execution. Check permissions.');
+    }
+
+    afterEffectsExecutor.executeSync(scriptFile, options);
+
+    const results = require(resultFile);
+    attachLineInfoOnError(results, scriptFile)
+    closeLogWatcher(tail)
+
+    return results;
 }
 
-/*******************************************************************/
-// EXPORTS
-/*******************************************************************/
+function closeLogWatcher(tail) {
+    setTimeout(() => tail.unwatch(), 2000)
+}
 
-module.exports = function() {
-  return executeSync.apply(null, arguments);
-};
+function readLineOfFile(file, lineNumber) {
+    const lines = fs.readFileSync(file, 'utf-8').split(/\r?\n/);
+    let lineCount = 1
+    for (const line of lines) {
+        if (lineCount++ === lineNumber) return line
+    }
+    return null
+}
 
-module.exports.execute = execute;
-module.exports.executeSync = executeSync;
-module.exports.create = create;
-module.exports.createSync = createSync;
-module.exports.options = options;
-module.exports.Command = Command;
+function attachLineInfoOnError(results, file) {
+    if (!results || results.result !== 'ERROR') return
+    const lineText = readLineOfFile(file, results.line)
+    results.lineText = lineText;
+}
 
-Object.defineProperty(module.exports, 'scriptsDir', {
-  //Pass in dummy command so we have access to the currently set program option, if one exists
-  get: () => platform.scriptsDir({ options: { program: module.exports.options.program }})
-});
-Object.preventExtensions(module.exports);
-Object.preventExtensions(module.exports.options);
+function makeAfterEffectsInterface(userOptions = {}) {
+    options = {...options, ...userOptions}
+    return {executeFunctionInAfterEffects}
+}
+
+module.exports.makeAfterEffectsInterface = makeAfterEffectsInterface
---
+++ sample-composition.js
@@ -0,0 +1,42 @@
+const makeAfterEffectsInterface = require('./index.js').makeAfterEffectsInterface
+
+// These options will be passed to the after effects process and our function will receive them as a paramter to the function
+const options = {
+    "assetFolder": "C:\\development\\projects\\videos",
+    "compositionMain": {
+        "name": "render main",
+        "layerOverlapSeconds": 2,
+        "videoLengthLimitSeconds": 120
+    },
+}
+
+const aeInterface = makeAfterEffectsInterface({afterEffectsPath: 'C:\\Program Files\\Adobe\\Adobe After Effects CC 2019'})
+const result = aeInterface.executeFunctionInAfterEffects(optionsFromNode => {
+    log.info('begin after effects script execution')
+
+    const {options} = optionsFromNode
+    const assetsInterface = makeAssetsInterface(app.project)
+    const projectInterface = makeProjectInterface(app.project)
+    const layersInterface = makeLayersInterface(assetsInterface.findByName(options.compositionMain.name))
+    const importedAssets = projectInterface.importAssets(options.assetFolder, '*.mp4')
+    
+    const videoEndTimes = []
+
+    log.info('creating layers')
+    layersInterface.addAll(importedAssets, options.compositionMain.layerOverlapSeconds, (layer, previousLayer) => {
+        log.info('  adding layer: ' + layer.name)
+
+        if (previousLayer) {
+            // make a list of the transition times to be used later
+            videoEndTimes.push(previousLayer.outPoint)
+        }
+
+        if (layer.outPoint > options.compositionMain.videoLengthLimitSeconds) return true // don't add anymore beyond time limit
+    })
+
+    log.info('After Effects finished processing script')
+    return videoEndTimes
+}, {options});
+
+// log list of video clip end times
+console.dir(result, {depth: null})
